fb71a9dfa6ef04c34d91d9b2629345abeae5030b
rubenszinho <62901197+rubenszinho@users.noreply.github.com>
2020-10-08 21:24:48 -0300

    up

--- Alg I/Mudam-se os Tempos, Mudam-se as Filas/main.c
+++ Alg I/Mudam-se os Tempos, Mudam-se as Filas/main.c
@@ -8,15 +8,16 @@ typedef struct{
     int agravante;
 }pessoa;
     int main(){
-        // int n, tam;
-        char funcao;
-        // scanf("%d", tam)
-        fgets(funcao, sizeof(funcao), stdin);
-        if(funcao == "SAI")
+        char funcao[10];
+        fgets(funcao, 10, stdin);
+        funcao[strcspn(funcao, "\n")] = 0;
+        if (strcmp(funcao,"SAI") == 0)
             printf("saiu");
-        else
-        {
-            printf("nao saiu");
-        }  
-    return 0;      
-    }   
+        else if (strcmp(funcao,"ENTRA") == 0)
+            printf("entrou");
+        else{
+            printf("invalido");
+            return 0;
+        }
+    return 0;
+    }
